96d305506f68f7d470d13c04b295b3c24d691412
JColeby <jonathan.2022jatc@gmail.com>
2025-09-11 13:36:49 -0600

    Implemented resetBoard, toString, equals, and hashCode in ChessBoard class. All tests have been passed

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -208,4 +208,9 @@ public class ChessPiece {
     public int hashCode() {
         return Objects.hash(pieceColor, type);
     }
+
+    @Override
+    public String toString() {
+        return  pieceColor + "." + type;
+    }
 }
